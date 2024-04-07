4d563db1ca0cfbde06dc0a50c17e69f7d41a0887
ayuaprliia <ayuagungaprilia@gmail.com>
2024-04-07 11:00:57 +0800

    modify greeting page

--- lib/views/greeting.dart
+++ lib/views/greeting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utility/colors.dart';
 
 class GreetingView extends StatefulWidget {
   const GreetingView({super.key});
@@ -10,12 +11,87 @@ class GreetingView extends StatefulWidget {
 class _GreetingViewState extends State<GreetingView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
-        child:Text(
-          "halo"
-          ) ,
+        child:Padding(
+          padding:const EdgeInsets.fromLTRB(8, 55, 8, 55),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 16.0),
+                child:  Text(
+                  "Hilangkan Batasan Berkomunikasi\ndengan Signify!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "PoppinsBold", 
+                    fontSize: 24
+                  ),
+                ),
+              ),
+               Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Belajar BISINDO itu mudah dan menyenangkan!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "PoppinsMedium", 
+                    fontSize: 16, 
+                    color: fontGrayColor
+                  ),
+               ),
+              ),
+               Padding(
+                  padding: const EdgeInsets.fromLTRB(55, 60, 55, 90),
+                 child: Image.asset('assets/images/greeting.png'),
+              ),
+              
+               //membuat button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:16),
+                  child: GestureDetector(
+                    onTap: (){
+                      print("masuk");
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(vertical:13.5),
+                      decoration: BoxDecoration(color: blueColor, 
+                      borderRadius: BorderRadius.circular(100)),
+                      child:  
+                      const Text("Masuk", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "PoppinsMedium", 
+                        fontSize: 16, 
+                        color: Colors.white),
+                        ),
+                    ),
+                  ),
+                ),
+                
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:16),
+                child: GestureDetector(
+                  onTap: (){
+                    print("daftar");
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical:13.5),
+                    decoration: BoxDecoration(color:Colors.white, borderRadius: BorderRadius.circular(100)),
+                    child: Text("Daftar", 
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "PoppinsMedium", fontSize: 16, color: blueColor),
+                    ),
+                ),
+              ),
+            )
+            ],
+          ),
         ),
+      ),
     );
   }
 }
